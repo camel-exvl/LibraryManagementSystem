424fd3f7589490a49a71585f8e2116f745e5e882
章越 <camel_zy@qq.com>
2023-05-06 00:08:04 +0800

    fix(Library类后端实现): 修复后端bug，通过全部测试
    1. 修复存储书时用book_id判断是否重复插入的bug，改为用类别、标题、出版社、出版年份、作者信息共同判断
    2. 修复所有函数出现异常不rollback的bug
    3. 修复无法存储书、卡时无法获取主键的bug 4. 修复所有函数中commit位置错误的bug
    5. 修复移除书、卡时不判断该书、卡是否存在的bug
    6. 修复查询书时无法模糊匹配的bug
    7. 修复查询书时查询条件不能存在空值的bug
    8. 修复借书时判断书是否存在的逻辑错误
    9. 修复借书时未将还书时间设空的bug
    10. 修复还书时不检查是否存在该条借书记录的bug
    11. 修复还书时不检查还书时间是否比借书时间早的bug
    12. 修复查询所有卡时不按card_id排序的bug
    13. 部分代码和逻辑的优化调整

--- src/main/java/LibraryManagementSystemImpl.java
+++ src/main/java/LibraryManagementSystemImpl.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,15 +31,20 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
     public ApiResult storeBook(Book book) {
         Connection conn = connector.getConn();
         try {
-            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM book WHERE book_id = ? FOR UPDATE");
-            stmt.setInt(1, book.getBookId());
+            PreparedStatement stmt = conn.prepareStatement(
+                    "SELECT * FROM book WHERE category = ? AND title = ? AND press = ? AND publish_year = ? AND author = ?");
+            stmt.setString(1, book.getCategory());
+            stmt.setString(2, book.getTitle());
+            stmt.setString(3, book.getPress());
+            stmt.setInt(4, book.getPublishYear());
+            stmt.setString(5, book.getAuthor());
             ResultSet rs = stmt.executeQuery();
-            commit(conn);
             if (rs.next()) {
-                return new ApiResult(false, "Book already exists");
+                throw new Exception("Book already exists");
             }
             stmt = conn.prepareStatement(
-                    "INSERT INTO book (category, title, press, publish_year, author, price, stock) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                    "INSERT INTO book (category, title, press, publish_year, author, price, stock) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, book.getCategory());
             stmt.setString(2, book.getTitle());
             stmt.setString(3, book.getPress());
@@ -48,10 +54,12 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
             stmt.setInt(7, book.getStock());
             stmt.executeUpdate();
             rs = stmt.getGeneratedKeys();
-            commit(conn);
             if (rs.next()) {
                 book.setBookId(rs.getInt(1));
+            } else {
+                throw new Exception("Failed to get book id");
             }
+            commit(conn);
         } catch (Exception e) {
             rollback(conn);
             return new ApiResult(false, e.getMessage());
@@ -66,13 +74,12 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM book WHERE book_id = ? FOR UPDATE");
             stmt.setInt(1, bookId);
             ResultSet rs = stmt.executeQuery();
-            commit(conn);
             if (!rs.next()) {
-                return new ApiResult(false, "Book not found");
+                throw new Exception("Book not exists");
             }
             int stock = rs.getInt("stock");
             if (stock + deltaStock < 0) {
-                return new ApiResult(false, "Stock not enough");
+                throw new Exception("Stock not enough");
             }
             stmt = conn.prepareStatement("UPDATE book SET stock = ? WHERE book_id = ?");
             stmt.setInt(1, stock + deltaStock);
@@ -90,13 +97,17 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
     public ApiResult storeBook(List<Book> books) {
         Connection conn = connector.getConn();
         try {
-            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM book WHERE book_id = ? FOR UPDATE");
+            PreparedStatement stmt = conn.prepareStatement(
+                    "SELECT * FROM book WHERE category = ? AND title = ? AND press = ? AND publish_year = ? AND author = ?");
             for (Book book : books) {
-                stmt.setInt(1, book.getBookId());
+                stmt.setString(1, book.getCategory());
+                stmt.setString(2, book.getTitle());
+                stmt.setString(3, book.getPress());
+                stmt.setInt(4, book.getPublishYear());
+                stmt.setString(5, book.getAuthor());
                 ResultSet rs = stmt.executeQuery();
-                commit(conn);
                 if (rs.next()) {
-                    return new ApiResult(false, "Book already exists");
+                    throw new Exception("Book already exists");
                 }
             }
             stmt = conn.prepareStatement(
@@ -114,12 +125,14 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
             }
             stmt.executeBatch();
             ResultSet rs = stmt.getGeneratedKeys();
-            commit(conn);
             for (Book book : books) {
                 if (rs.next()) {
                     book.setBookId(rs.getInt(1));
+                } else {
+                    throw new Exception("Failed to get book id");
                 }
             }
+            commit(conn);
         } catch (Exception e) {
             rollback(conn);
             return new ApiResult(false, e.getMessage());
@@ -135,13 +148,15 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
                     .prepareStatement("SELECT * FROM borrow WHERE book_id = ? AND return_time = 0 FOR UPDATE");
             stmt.setInt(1, bookId);
             ResultSet rs = stmt.executeQuery();
-            commit(conn);
             if (rs.next()) {
-                return new ApiResult(false, "Book is borrowed");
+                throw new Exception("Book is borrowed");
             }
             stmt = conn.prepareStatement("DELETE FROM book WHERE book_id = ?");
             stmt.setInt(1, bookId);
-            stmt.executeUpdate();
+            Integer affectedRows = stmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new Exception("Book not exists");
+            }
             commit(conn);
         } catch (Exception e) {
             rollback(conn);
@@ -178,7 +193,7 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
         List<Book> books = new ArrayList<>();
         try {
             StringBuffer sql = new StringBuffer(
-                    "SELECT * FROM book WHERE (category = ? OR ? IS NULL) AND (title = ? OR ? IS NULL) AND (press = ? OR ? IS NULL) AND (publish_year >= ? OR ? IS NULL) AND (publish_year <= ? OR ? IS NULL) AND (author = ? OR ? IS NULL) AND (price >= ? OR ? IS NULL) AND (price <= ? OR ? IS NULL)");
+                    "SELECT * FROM book WHERE (category = ? OR ? IS NULL) AND (title like ? OR ? IS NULL) AND (press like ? OR ? IS NULL) AND (publish_year >= ? OR ? IS NULL) AND (publish_year <= ? OR ? IS NULL) AND (author like ? OR ? IS NULL) AND (price >= ? OR ? IS NULL) AND (price <= ? OR ? IS NULL)");
             if (conditions.getSortBy() != Book.SortColumn.BOOK_ID) {
                 sql.append(
                         " ORDER BY " + conditions.getSortBy().toString() + " " + conditions.getSortOrder().toString()
@@ -187,24 +202,64 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
                 sql.append(" ORDER BY book_id " + conditions.getSortOrder().toString());
             }
             PreparedStatement pstmt = conn.prepareStatement(sql.toString());
-            pstmt.setString(1, conditions.getCategory());
-            pstmt.setString(2, conditions.getCategory());
-            pstmt.setString(3, conditions.getTitle());
-            pstmt.setString(4, conditions.getTitle());
-            pstmt.setString(5, conditions.getPress());
-            pstmt.setString(6, conditions.getPress());
-            pstmt.setInt(7, conditions.getMinPublishYear());
-            pstmt.setInt(8, conditions.getMinPublishYear());
-            pstmt.setInt(9, conditions.getMaxPublishYear());
-            pstmt.setInt(10, conditions.getMaxPublishYear());
-            pstmt.setString(11, conditions.getAuthor());
-            pstmt.setString(12, conditions.getAuthor());
-            pstmt.setDouble(13, conditions.getMinPrice());
-            pstmt.setDouble(14, conditions.getMinPrice());
-            pstmt.setDouble(15, conditions.getMaxPrice());
-            pstmt.setDouble(16, conditions.getMaxPrice());
+            if (conditions.getCategory() == null) {
+                pstmt.setNull(1, Types.VARCHAR);
+                pstmt.setNull(2, Types.VARCHAR);
+            } else {
+                pstmt.setString(1, conditions.getCategory());
+                pstmt.setString(2, conditions.getCategory());
+            }
+            if (conditions.getTitle() == null) {
+                pstmt.setNull(3, Types.VARCHAR);
+                pstmt.setNull(4, Types.VARCHAR);
+            } else {
+                pstmt.setString(3, '%' + conditions.getTitle() + '%');
+                pstmt.setString(4, '%' + conditions.getTitle() + '%');
+            }
+            if (conditions.getPress() == null) {
+                pstmt.setNull(5, Types.VARCHAR);
+                pstmt.setNull(6, Types.VARCHAR);
+            } else {
+                pstmt.setString(5, '%' + conditions.getPress() + '%');
+                pstmt.setString(6, '%' + conditions.getPress() + '%');
+            }
+            if (conditions.getMinPublishYear() == null) {
+                pstmt.setNull(7, Types.INTEGER);
+                pstmt.setNull(8, Types.INTEGER);
+            } else {
+                pstmt.setInt(7, conditions.getMinPublishYear());
+                pstmt.setInt(8, conditions.getMinPublishYear());
+            }
+            if (conditions.getMaxPublishYear() == null) {
+                pstmt.setNull(9, Types.INTEGER);
+                pstmt.setNull(10, Types.INTEGER);
+            } else {
+                pstmt.setInt(9, conditions.getMaxPublishYear());
+                pstmt.setInt(10, conditions.getMaxPublishYear());
+            }
+            if (conditions.getAuthor() == null) {
+                pstmt.setNull(11, Types.VARCHAR);
+                pstmt.setNull(12, Types.VARCHAR);
+            } else {
+
+                pstmt.setString(11, '%' + conditions.getAuthor() + '%');
+                pstmt.setString(12, '%' + conditions.getAuthor() + '%');
+            }
+            if (conditions.getMinPrice() == null) {
+                pstmt.setNull(13, Types.DOUBLE);
+                pstmt.setNull(14, Types.DOUBLE);
+            } else {
+                pstmt.setDouble(13, conditions.getMinPrice());
+                pstmt.setDouble(14, conditions.getMinPrice());
+            }
+            if (conditions.getMaxPrice() == null) {
+                pstmt.setNull(15, Types.DOUBLE);
+                pstmt.setNull(16, Types.DOUBLE);
+            } else {
+                pstmt.setDouble(15, conditions.getMaxPrice());
+                pstmt.setDouble(16, conditions.getMaxPrice());
+            }
             ResultSet rs = pstmt.executeQuery();
-            commit(conn);
             while (rs.next()) {
                 Book book = new Book();
                 book.setBookId(rs.getInt("book_id"));
@@ -217,6 +272,7 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
                 book.setStock(rs.getInt("stock"));
                 books.add(book);
             }
+            commit(conn);
         } catch (Exception e) {
             rollback(conn);
             return new ApiResult(false, e.getMessage());
@@ -231,27 +287,24 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
             PreparedStatement pstmt = conn.prepareStatement(
                     "SELECT * FROM book WHERE book_id = ? AND stock > 0 FOR UPDATE");
             pstmt.setInt(1, borrow.getBookId());
-            pstmt.addBatch();
+            ResultSet rs = pstmt.executeQuery();
+            if (!rs.next()) {
+                throw new Exception("No such book or no stock");
+            }
             pstmt = conn.prepareStatement(
                     "SELECT * FROM borrow WHERE card_id = ? AND book_id = ? AND return_time = 0 FOR UPDATE");
             pstmt.setInt(1, borrow.getCardId());
             pstmt.setInt(2, borrow.getBookId());
-            pstmt.addBatch();
-            pstmt.executeBatch();
-            ResultSet rs = pstmt.getResultSet();
-            if (!rs.next()) {
-                return new ApiResult(false, "No such book or no stock");
-            }
-            rs.next();
-            if (!rs.next()) {
-                return new ApiResult(false, "Have borrowed this book");
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                throw new Exception("This book has been borrowed by this card");
             }
             pstmt = conn.prepareStatement(
                     "INSERT INTO borrow (card_id, book_id, borrow_time, return_time) VALUES (?, ?, ?, ?)");
             pstmt.setInt(1, borrow.getCardId());
             pstmt.setInt(2, borrow.getBookId());
             pstmt.setLong(3, borrow.getBorrowTime());
-            pstmt.setLong(4, borrow.getReturnTime());
+            pstmt.setLong(4, 0);
             pstmt.executeUpdate();
             pstmt = conn.prepareStatement("UPDATE book SET stock = stock - 1 WHERE book_id = ?");
             pstmt.setInt(1, borrow.getBookId());
@@ -269,6 +322,17 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
         Connection conn = connector.getConn();
         try {
             PreparedStatement pstmt = conn.prepareStatement(
+                    "SELECT * FROM borrow WHERE card_id = ? AND book_id = ? AND return_time = 0 FOR UPDATE");
+            pstmt.setInt(1, borrow.getCardId());
+            pstmt.setInt(2, borrow.getBookId());
+            ResultSet rs = pstmt.executeQuery();
+            if (!rs.next()) {
+                throw new Exception("No such borrow record");
+            }
+            if (borrow.getReturnTime() <= rs.getLong("borrow_time")) {
+                throw new Exception("Return time is earlier than borrow time");
+            }
+            pstmt = conn.prepareStatement(
                     "UPDATE borrow SET return_time = ? WHERE card_id = ? AND book_id = ? AND return_time = 0");
             pstmt.setLong(1, borrow.getReturnTime());
             pstmt.setInt(2, borrow.getCardId());
@@ -294,7 +358,6 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
                     "SELECT * FROM borrow NATURAL JOIN book WHERE card_id = ? ORDER BY borrow_time DESC, book_id ASC");
             pstmt.setInt(1, cardId);
             ResultSet rs = pstmt.executeQuery();
-            commit(conn);
             while (rs.next()) {
                 Book book = new Book(rs.getString("category"), rs.getString("title"),
                         rs.getString("press"), rs.getInt("publish_year"), rs.getString("author"),
@@ -306,6 +369,7 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
                 Item item = new Item(rs.getInt("card_id"), book, borrow);
                 items.add(item);
             }
+            commit(conn);
         } catch (Exception e) {
             rollback(conn);
             return new ApiResult(false, e.getMessage());
@@ -322,23 +386,23 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
             pstmt.setString(1, card.getName());
             pstmt.setString(2, card.getDepartment());
             pstmt.setString(3, card.getType().getStr());
-            pstmt.executeQuery();
-            ResultSet rs = pstmt.getResultSet();
-            commit(conn);
+            ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
-                return new ApiResult(false, "Card already exists");
+                throw new Exception("This card has been registered");
             }
             pstmt = conn.prepareStatement(
-                    "INSERT INTO card (name, department, type) VALUES (?, ?, ?)");
+                    "INSERT INTO card (name, department, type) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
             pstmt.setString(1, card.getName());
             pstmt.setString(2, card.getDepartment());
             pstmt.setString(3, card.getType().getStr());
-            pstmt.executeQuery();
+            pstmt.executeUpdate();
             rs = pstmt.getGeneratedKeys();
-            commit(conn);
             if (rs.next()) {
                 card.setCardId(rs.getInt(1));
+            } else {
+                throw new Exception("Failed to get card id");
             }
+            commit(conn);
         } catch (Exception e) {
             rollback(conn);
             return new ApiResult(false, e.getMessage());
@@ -354,14 +418,16 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
                     "SELECT * FROM borrow WHERE card_id = ? AND return_time = 0 FOR UPDATE");
             pstmt.setInt(1, cardId);
             ResultSet rs = pstmt.executeQuery();
-            commit(conn);
             if (rs.next()) {
-                return new ApiResult(false, "Card has borrowed books");
+                throw new Exception("This card has borrowed books");
             }
             pstmt = conn.prepareStatement(
                     "DELETE FROM card WHERE card_id = ?");
             pstmt.setInt(1, cardId);
-            pstmt.executeUpdate();
+            Integer affectedRows = pstmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new Exception("No such card");
+            }
             commit(conn);
         } catch (Exception e) {
             rollback(conn);
@@ -376,14 +442,14 @@ public class LibraryManagementSystemImpl implements LibraryManagementSystem {
         List<Card> cards = new ArrayList<>();
         try {
             PreparedStatement pstmt = conn.prepareStatement(
-                    "SELECT * FROM card");
+                    "SELECT * FROM card ORDER BY card_id ASC");
             ResultSet rs = pstmt.executeQuery();
-            commit(conn);
             while (rs.next()) {
                 Card card = new Card(rs.getInt("card_id"), rs.getString("name"),
-                        rs.getString("department"), CardType.valueOf(rs.getString("type")));
+                        rs.getString("department"), CardType.values(rs.getString("type")));
                 cards.add(card);
             }
+            commit(conn);
         } catch (Exception e) {
             rollback(conn);
             return new ApiResult(false, e.getMessage());
